d7c843166f4f9774696a6f9a69447fece0a978db
Iqbal Mohammed Rhidwan <rhidwan@Iqbals-MacBook-Air.local>
2020-01-25 18:40:59 +0600

    Updates

--- precache-manifest.c443088a8ec79cae18c5797324359687.js
+++ precache-manifest.c443088a8ec79cae18c5797324359687.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "604a44a3c71de5730890103db4afd93b",
+    "revision": "668331effb477f2117f39b99d6ffa0f1",
     "url": "/blog/index.html"
   },
   {
-    "revision": "b1c9eb1d9c8e2dbbe80e",
+    "revision": "a5b72adb413992ca0fd4",
     "url": "/blog/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "825df8dd8e8236192280",
+    "revision": "bb85d121ddc8581863cf",
     "url": "/blog/static/css/main.5de3e6ca.chunk.css"
   },
   {
-    "revision": "b1c9eb1d9c8e2dbbe80e",
-    "url": "/blog/static/js/2.2d88fc61.chunk.js"
+    "revision": "a5b72adb413992ca0fd4",
+    "url": "/blog/static/js/2.438e8ee2.chunk.js"
   },
   {
     "revision": "86afaa925e0f432774bf95b19b6933fb",
-    "url": "/blog/static/js/2.2d88fc61.chunk.js.LICENSE"
+    "url": "/blog/static/js/2.438e8ee2.chunk.js.LICENSE"
   },
   {
-    "revision": "825df8dd8e8236192280",
-    "url": "/blog/static/js/main.338ad0fb.chunk.js"
+    "revision": "bb85d121ddc8581863cf",
+    "url": "/blog/static/js/main.28d648d3.chunk.js"
   },
   {
     "revision": "cbfd563c8600f0c401a0",
